819900d9e40de7b7d7d0609caa88268c65761636
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2025-09-02 22:08:14 +0530

    Add convolution.py and cnn.png for convolution demonstration

---
+++ 2025-09-01-Kurunagala-Sri-Lanka/convolution.py
@@ -0,0 +1,33 @@
+import marimo
+
+__generated_with = "0.13.11"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import polars as pl
+    import numpy as np
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""![](D:\PROJECTS\AI-WORKSHOPS\AI-Sessions\2025-09-01-Kurunagala-Sri-Lanka\public\cnn.png)""")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""# What is Convolution""")
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
